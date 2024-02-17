949cf60a36f9fcbd34a29e24b6d991dd7f5af9ae
ramintheredmn <redcrossprog@gmail.com>
2024-02-17 23:14:59 +0330

    all

--- backend/webscraping.py
+++ backend/webscraping.py
@@ -37,7 +37,7 @@ def webScrape(smiles):
     submitButton = driver.find_element(By.ID, "submitButton")
     data = []
 
-    for i, radio in enumerate(radio_buttons[0:-1:10]):
+    for i, radio in enumerate(radio_buttons[11:]):
         radio.click()
         submitButton.click()
         driver.implicitly_wait(30)
